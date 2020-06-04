0620c60cf94cbc8cfb200ab2ce04be951110e66e
tikimcrzx <tikimioo723@gmail.com>
2020-06-04 12:20:22 -0700

    [FIX] cambio de context -> content-type

--- routes/verPerfil.go
+++ routes/verPerfil.go
@@ -21,7 +21,7 @@ func VerPerfil(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("context-type", "application/json")
+	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(perfil)
 }
